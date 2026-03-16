cbe9821ba8226831d620c9236fd6824f6d8798b9
Erick <erick.lima195@outlook.com>
2026-03-16 10:11:28 -0300

    Initial Commit

---
+++ VellantiTests/VellantiTests.swift
@@ -0,0 +1,17 @@
+//
+//  VellantiTests.swift
+//  VellantiTests
+//
+//  Created by Erick Maicon Lima de Almeida on 16/03/26.
+//
+
+import Testing
+@testable import Vellanti
+
+struct VellantiTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
