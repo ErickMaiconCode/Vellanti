14fdb5fbf430bfca6f6cddef90e45af1247c2d6e
Erick <erick.lima195@outlook.com>
2026-03-16 10:11:28 -0300

    Initial Commit

---
+++ Vellanti/VellantiApp.swift
@@ -0,0 +1,17 @@
+//
+//  VellantiApp.swift
+//  Vellanti
+//
+//  Created by Erick Maicon Lima de Almeida on 16/03/26.
+//
+
+import SwiftUI
+
+@main
+struct VellantiApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
